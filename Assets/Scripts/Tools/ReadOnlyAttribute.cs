2bd9545c32ea04a2f21d014c055d264ce5fa923f
Medieval Apple <114710036+MedievalApple@users.noreply.github.com>
2026-07-08 08:57:47 -0400

    Test

--- Assets/Scripts/Tools/ReadOnlyAttribute.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Used exclusively for making read-only text fields in the inspector for custom varaibles.
-/// </summary>
-public class ReadOnlyAttribute : PropertyAttribute { }
